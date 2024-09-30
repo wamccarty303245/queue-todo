ba724675878ea605d33fd26b8b8b3b4eb5064ece
William McCarty <wamccarty303245@gmail.com>
2024-09-30 14:31:31 -0500

    Added function outlines so we can get started on them (inside of queue_functions.cpp)

--- queue_functions.cpp
+++ queue_functions.cpp
@@ -1,3 +1,19 @@
 //
 // Created by William McCarty on 6/26/24.
 //
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+void displayList(){
+    //need to do
+}
+void addTask (){
+    //need to do
+}
+
+void remTask(){
+    //need to do
+}
+
--- queue_head.h
+++ queue_head.h
@@ -1,6 +1,12 @@
 //
 // Created by William McCarty on 6/26/24.
 //
+#include <iostream>
+
+
+
+
+
 
 #ifndef TODO_QUEUE_HEAD_H
 #define TODO_QUEUE_HEAD_H
